552dfc7f02040188d486c6fb4a3223d46ec097ed
BernardoModerno <bernardo.moderno@gmail.com>
2022-11-16 15:49:36 -0300

    criando a action de cadastro de serviço

--- admin-ediaristas/src/main/java/com/admin/ediaristas/web/controllers/ServicoController.java
+++ admin-ediaristas/src/main/java/com/admin/ediaristas/web/controllers/ServicoController.java
@@ -1,25 +1,36 @@
 package com.admin.ediaristas.web.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.admin.ediaristas.core.enums.Icone;
 import com.admin.ediaristas.core.models.Servico;
+import com.admin.ediaristas.core.repositories.ServicoRepository;
 
 @Controller
 @RequestMapping("/admin/servicos")
 public class ServicoController {
 
+    @Autowired
+    private ServicoRepository repository;
+
     @GetMapping("/cadastrar")
     public ModelAndView cadastrar() {
         var modelAndView = new ModelAndView("admin/servico/form");
-
         modelAndView.addObject("servico", new Servico());
-
         return modelAndView;
+    }
+
+    @PostMapping("/cadastrar")
+    public String cadastrar(Servico servico) {
+        repository.save(servico);
+
+        return "redirect:/admin/servicos/cadastrar";
     }
 
     @ModelAttribute("icones")
