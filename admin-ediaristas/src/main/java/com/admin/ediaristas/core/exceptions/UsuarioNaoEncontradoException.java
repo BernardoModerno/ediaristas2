f1039a5a24e3898e7cd185b174f8ea44d65a2443
BernardoModerno <bernardo.moderno@gmail.com>
2022-11-17 15:34:13 -0300

    criando a exclusão de usuário

---
+++ admin-ediaristas/src/main/java/com/admin/ediaristas/core/exceptions/UsuarioNaoEncontradoException.java
@@ -0,0 +1,11 @@
+package com.admin.ediaristas.core.exceptions;
+
+import javax.persistence.EntityNotFoundException;
+
+public class UsuarioNaoEncontradoException extends EntityNotFoundException {
+
+    public UsuarioNaoEncontradoException(String message) {
+        super(message);
+    }
+
+}
