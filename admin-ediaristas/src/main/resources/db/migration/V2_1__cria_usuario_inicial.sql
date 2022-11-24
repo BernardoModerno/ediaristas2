fd0cb349598d39e0c742af7c8b8dbc6ed551eefa
BernardoModerno <bernardo.moderno@gmail.com>
2022-11-24 19:58:44 -0300

    criando migrações para versionar o banco de dados

---
+++ admin-ediaristas/src/main/resources/db/migration/V2_1__cria_usuario_inicial.sql
@@ -0,0 +1,8 @@
+INSERT INTO `usuario`
+VALUES (
+        1,
+        'admin@mail.com',
+        'Usuário ADMIN',
+        '$2a$12$zA3b1harhRSBBdT/kGORW.0wMuS9JwIN4NnLFKgXlDFOwqoMp4tca',
+        'ADMIN'
+    );
