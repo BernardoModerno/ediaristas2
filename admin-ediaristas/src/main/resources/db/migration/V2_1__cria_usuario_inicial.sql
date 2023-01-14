ab3cb1d06fc31ba37fc25dd490c232b1ec215c31
BernardoModerno <bernardo.moderno@gmail.com>
2023-01-13 21:46:21 -0300

    Buscar diaristas por cep, cors

--- admin-ediaristas/src/main/resources/db/migration/V2_1__cria_usuario_inicial.sql
+++ admin-ediaristas/src/main/resources/db/migration/V2_1__cria_usuario_inicial.sql
@@ -1,1 +1,8 @@
-INSERT INTO `usuario` VALUES (2,'bernardo.moderno@mail.com','Usuário ADMIN','$2a$12$iHXRGmRVWOSA/DzqgR8hxuBgBCdsLmRi/hT7xY18o2Jd0BqdbsUqy','ADMIN');
+INSERT INTO `usuario`
+VALUES (
+        2,
+        'bernardo.moderno@mail.com',
+        'Usuário ADMIN',
+        '$2a$12$iHXRGmRVWOSA/DzqgR8hxuBgBCdsLmRi/hT7xY18o2Jd0BqdbsUqy',
+        'ADMIN'
+    );
